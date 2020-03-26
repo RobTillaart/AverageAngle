f16f204f0a86b58f63be270171d07816297f2d92
rob tillaart <rob.tillaart@gmail.com>
2020-03-26 19:51:09 +0100

    0.1.3 version

---
+++ AverageAngle.cpp
@@ -0,0 +1,65 @@
+//
+//    FILE: AverageAngle.cpp
+//  AUTHOR: Rob Tillaart
+// VERSION: 0.1.3
+// PURPOSE: class for averaging angles
+//     URL: https://github.com/RobTillaart/AverageAngle
+//
+// HISTORY:
+//
+// 0.1.0 2017-11-21 initial version
+// 0.1.1 2017-12-09 fixed negative values of average
+// 0.1.2 2018-03-30 added getAverageLength, getTotalLength + zero-test
+// 0.1.3 2020-03-26 #pragma once; removed pre 1.00 support; readme.md
+
+#include "AverageAngle.h"
+
+
+AverageAngle::AverageAngle(const enum AngleType type)
+{
+  _type = type;
+  reset();
+}
+
+void AverageAngle::add(float alpha, float length)
+{
+  if (_type == AverageAngle::DEGREES )
+  {
+    alpha *= DEG_TO_RAD; 				// (PI / 180.0);
+  }
+  _sumx += (cos(alpha) * length);
+  _sumy += (sin(alpha) * length);
+  _count++;
+}
+
+void AverageAngle::reset()
+{
+  _sumx = 0;
+  _sumy = 0;
+  _count = 0;
+}
+
+float AverageAngle::getAverage()
+{
+  float angle = atan2(_sumy, _sumx);
+  if (angle < 0) angle += TWO_PI;		//	(PI * 2);
+  if (_type == AverageAngle::DEGREES )
+  {
+    angle *=  RAD_TO_DEG; 				// (180.0 / PI);
+  }
+  return angle;
+}
+
+float AverageAngle::getTotalLength()
+{
+  if (_count == 0) return 0;
+  return hypot(_sumy, _sumx);
+}
+
+float AverageAngle::getAverageLength()
+{
+  if (_count == 0) return 0;
+  return hypot(_sumy, _sumx) / _count;
+}
+
+// -- END OF FILE --
---
+++ AverageAngle.h
@@ -0,0 +1,41 @@
+#pragma once
+//
+//    FILE: AverageAngle.h
+//  AUTHOR: Rob dot Tillaart at gmail dot com
+// VERSION: 0.1.3
+// PURPOSE: class for averaging angles
+// HISTORY: See AverageAngle.cpp
+//
+// Released to the public domain
+//
+
+#include "math.h"
+#include "Arduino.h"
+
+#define AVERAGE_ANGLE_LIB_VERSION "0.1.3"
+
+class AverageAngle
+{
+public:
+    enum AngleType { DEGREES, RADIANS };
+
+    AverageAngle(const enum AngleType type = DEGREES);
+
+    void 	 add(float alpha, float length = 1.0);
+    void 	 reset();
+    uint32_t count() { return _count; };
+    float    getAverage();
+
+    float    getTotalLength();
+    float    getAverageLength();
+
+    enum     AngleType type() { return _type; };
+
+private:
+    enum AngleType _type;
+    float _sumx;
+    float _sumy;
+    uint32_t _count;
+};
+
+// -- END OF FILE --
