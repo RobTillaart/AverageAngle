2fcdeb8078fecac127a3c761010bea02b0028ea1
rob tillaart <rob.tillaart@gmail.com>
2021-10-18 17:29:07 +0200

    update build-CI, add gradians

--- test/unit_test_001.cpp
+++ test/unit_test_001.cpp
@@ -19,6 +19,7 @@
 // assertFalse(actual)
 // assertNull(actual)
 
+
 #include <ArduinoUnitTests.h>
 
 #include "AverageAngle.h"
@@ -32,17 +33,30 @@ unittest_teardown()
 {
 }
 
+
 unittest(test_constructor)
 {
+  fprintf(stderr, "AVERAGE_ANGLE_LIB_VERSION: %s\n", (char*) AVERAGE_ANGLE_LIB_VERSION);
+
   AverageAngle dd(AverageAngle::DEGREES);
   AverageAngle rr(AverageAngle::RADIANS);
-  
+  AverageAngle gg(AverageAngle::GRADIANS);
+
   assertEqual(AverageAngle::DEGREES, dd.type());
   assertEqual(AverageAngle::RADIANS, rr.type());
+  assertEqual(AverageAngle::GRADIANS, gg.type());
+
+  assertEqualFloat(PI / 180.0, DEG_TO_RAD, 0.00001);
+  assertEqualFloat(180.0 / PI, RAD_TO_DEG, 0.00001);
+  assertEqualFloat(PI / 200.0, GRAD_TO_RAD, 0.00001);
+  assertEqualFloat(200.0 / PI, RAD_TO_GRAD, 0.00001);
 }
+
 
 unittest(test_degrees)
 {
+  fprintf(stderr, "AVERAGE_ANGLE_LIB_VERSION: %s\n", (char*) AVERAGE_ANGLE_LIB_VERSION);
+
   AverageAngle aa(AverageAngle::DEGREES);
 
   for (int i = 0; i < 10; i++)
@@ -61,6 +75,55 @@ unittest(test_degrees)
   diff = abs(1 - aa.getAverageLength());
   assertMoreOrEqual(0.01, diff);
 }
+
+
+unittest(test_radians)
+{
+  fprintf(stderr, "AVERAGE_ANGLE_LIB_VERSION: %s\n", (char*) AVERAGE_ANGLE_LIB_VERSION);
+
+  AverageAngle aa(AverageAngle::RADIANS);
+
+  for (int i = 0; i < 10; i++)
+  {
+    int n = aa.add(i);
+    assertEqual(i + 1, n);
+  }
+  assertEqual(10,  aa.count());
+  assertEqual(4.5, aa.getAverage());
+
+  fprintf(stderr, "getTotalLength()\n");
+  float diff = abs(10 - aa.getTotalLength());
+  assertMoreOrEqual(0.2, diff);
+  
+  fprintf(stderr, "getAverageLength()\n");
+  diff = abs(1 - aa.getAverageLength());
+  assertMoreOrEqual(0.01, diff);
+}
+
+
+unittest(test_gradians)
+{
+  fprintf(stderr, "AVERAGE_ANGLE_LIB_VERSION: %s\n", (char*) AVERAGE_ANGLE_LIB_VERSION);
+
+  AverageAngle aa(AverageAngle::GRADIANS);
+
+  for (int i = 0; i < 10; i++)
+  {
+    int n = aa.add(i);
+    assertEqual(i + 1, n);
+  }
+  assertEqual(10,  aa.count());
+  assertEqual(4.5, aa.getAverage());
+
+  fprintf(stderr, "getTotalLength()\n");
+  float diff = abs(10 - aa.getTotalLength());
+  assertMoreOrEqual(0.2, diff);
+  
+  fprintf(stderr, "getAverageLength()\n");
+  diff = abs(1 - aa.getAverageLength());
+  assertMoreOrEqual(0.01, diff);
+}
+
 
 unittest_main()
 
